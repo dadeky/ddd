1b0dcb80cdc4faef3c245793ab1fb511b8e7d06c
dvorski <dadeky@gmail.com>
2017-03-10 08:33:50 +0100

    DomainException

---
+++ src/Ddd/Exception/DomainException.php
@@ -0,0 +1,18 @@
+<?php
+namespace Ddd\Exception;
+
+class DomainException extends \Exception
+{
+	private $parameters = array();
+	
+	public function __construct($message, $parameters)
+	{
+		$this->parameters = $parameters;
+		parent::__construct($message);
+	}
+	
+	public function getParameters()
+	{
+		return $this->parameters;
+	}
+}
